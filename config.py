0db25ba94304c863e26d74a3150eb42c6d24bb52
User <user@MacBook-Air-User.local>
2025-07-08 16:14:07 +0500

    Сохранил изменения перед rebase

--- config.py
+++ config.py
@@ -1,7 +1,19 @@
 import os
 
+# Токен Telegram-бота (устанавливается через переменные окружения)
 TOKEN = os.getenv("BOT_TOKEN")
-CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME")
-WEBHOOK_HOST = os.getenv("https://kukuruzik.onrender.com")  # зададим позже на Render
+
+# Имя Telegram-канала без @
+CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME", "kukuruzikuz")
+
+# ID админа (устанавливается через переменные окружения или используется значение по умолчанию)
+ADMIN_ID = int(os.getenv("ADMIN_ID", "2071181"))
+
+# URL вебхука (Render сам задаёт это значение через переменную RENDER_EXTERNAL_URL)
+WEBHOOK_HOST = os.getenv("RENDER_EXTERNAL_URL")
 WEBHOOK_PATH = "/webhook"
 WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+
+# Настройки сервера
+WEBAPP_HOST = "0.0.0.0"
+WEBAPP_PORT = int(os.getenv("PORT", default=8000))
