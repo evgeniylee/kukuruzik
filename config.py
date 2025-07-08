b23bf4883f7397b00bbab05eb49578a26c48a418
evgeniylee <48210635+evgeniylee@users.noreply.github.com>
2025-07-08 15:28:31 +0500

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 TOKEN = os.getenv("BOT_TOKEN")
 CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME")
-WEBHOOK_HOST = os.getenv("WEBHOOK_HOST")  # зададим позже на Render
+WEBHOOK_HOST = os.getenv("https://kukuruzik.onrender.com")  # зададим позже на Render
 WEBHOOK_PATH = "/webhook"
 WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
